4e94f6315be11654895bd04b4ab77d746f31b4cc
David Sjunnesson <sjunnesson@gmail.com>
2014-02-24 15:07:34 -0800

    node app

---
+++ node/app.js
@@ -0,0 +1,47 @@
+var applescript = require("applescript");
+var Kaiseki = require('kaiseki');
+
+
+var PARSE_APP_ID = "X9cQUkyFkubVnkM3s5RBEadjPo4lEcBwnnMv0Vjf";
+var PARSE_REST_KEY = "vinuIKiTgQdSi7RDDGZGPUxaIXVaGMVl7qaKktpw";
+var kaiseki = new Kaiseki(PARSE_APP_ID, PARSE_REST_KEY);
+
+
+function launchStream(stream) {
+	var script = 'tell application "Google Chrome" \r set windowList to every tab of every window whose URL starts with "http://www.netflix.com" \r repeat with tabList in windowList \r set tabList to tabList as any \r repeat with tabItr in tabList \r set tabItr to tabItr as any \r delete tabItr \r end repeat \r end repeat \r open location "http://www.netflix.com/WiPlayer?movieid=' + stream + '" \r tell window 1 to enter presentation mode \r end tell';
+
+	applescript.execString(script, function(err, rtn) {
+		if (err) {
+			console.log(err); // Something went wrong!
+		}
+	});
+}
+
+function getNetflixID(key) {
+	var params = {
+		where: {
+			key: key
+		}
+	};
+	kaiseki.getObjects("Netflix_movies", params, function(err, res, body, success) {
+		if (success) {
+			console.log('found object = ', body);
+			if (body.length > 0) {
+				launchStream(body[0].movie_id);
+			}else{
+				console.log("Key not found in parse");
+				launchSetupPage(key);
+			}
+		}
+	});
+}
+
+function launchSetupPage(key){
+
+}
+
+getNetflixID("1");
+
+setInterval(function() {
+	getNetflixID("2");
+}, 10000);
